53902439636d65eb0a42c0e110c7ca6793e2529b
Serge <varvar666@gmail.com>
2024-04-23 14:53:20 +0300

    move drivers to near folder

--- base_setup.cmake
+++ base_setup.cmake
@@ -10,14 +10,14 @@ set(DEVICE 			"STM32F446RE")
 
 set(inc_dirs 
 	"inc/"
-	"Drivers/CMSIS/Include/"
-	"Drivers/CMSIS/Device/ST/STM32F4xx/Include/"
-	"Drivers/src/"
+	"../Drivers/CMSIS/Include/"
+	"../Drivers/CMSIS/Device/ST/STM32F4xx/Include/"
+	"../Drivers/src/"
 	)
 
 set(src_dirs
 	"src/"
-	"Drivers/src/"
+	"../Drivers/src/"
 	"Startup/"
 	)
 
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 
+
 int main()
 {
 	System_F4::Init();
