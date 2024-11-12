b0c49fdae328befe54bee8c758f1eac4c32dbb15
VarvaR <varvar666@gmail.com>
2024-11-12 09:14:22 +0300

    remake cmake.txt

--- base_setup.cmake
+++ base_setup.cmake
@@ -1,27 +1,81 @@
 
 set(PROJECT_NAME ${prj_name})
-
-set(CXX_STD_VERSION 20)
 set(VER 1.0)
 set(DESC "Base for STM32 cmake")
 set(DEVICE_FAMILY	"STM32F4")
 set(DEVICE_SERIES	"STM32F446xx")
 set(DEVICE 			"STM32F446RE")
 
-set(inc_dirs 
-	"inc/"
-	"Drivers/CMSIS/Include/"
-	"Drivers/CMSIS/Device/ST/STM32F4xx/Include/"
-	"Drivers/src/"
-	"Drivers/src/tools"
-	)
+# Core MCU flags, CPU, instruction set and FPU setup
+set(cpu_PARAMS
+    -mthumb
 
-set(src_dirs
-	"src/"
-	"Drivers/src/"
-	"Drivers/src/tools"
-	"Startup/"
-	)
+    # Other parameters
+    # -mcpu, -mfloat, -mfloat-abi, ...
+    -mcpu=cortex-m4
+	-mfpu=fpv4-sp-d16
+	-mfloat-abi=hard
+	
+)
 
-set(linker_script "${CMAKE_CURRENT_LIST_DIR}/STM32F446RETX.ld")
-set(linker_params "-Wl,--no-warn-rwx-segment")
+# Linker script
+set(linker_script_SRC
+    ${CMAKE_CURRENT_SOURCE_DIR}/stm32f446retx_FLASH.ld
+)
+
+# Compiler options 
+set(compiler_OPTS	# => target_compile_options
+)
+
+# Linker options
+set(linker_OPTS 	# => target_link_options
+)
+
+# Linker script
+set(linker_script_SRC
+	${CMAKE_CURRENT_SOURCE_DIR}/STM32F446RETX.ld
+)
+
+# Sources
+set(sources_SRCS
+	${CMAKE_CURRENT_SOURCE_DIR}/src/main.cpp
+	${CMAKE_CURRENT_SOURCE_DIR}/Startup/startup.c
+	${CMAKE_CURRENT_SOURCE_DIR}/Startup/vector.c
+	${CMAKE_CURRENT_SOURCE_DIR}/Startup/sysmem.c
+	${CMAKE_CURRENT_SOURCE_DIR}/Startup/syscalls.c
+
+	${CMAKE_CURRENT_SOURCE_DIR}/Drivers/src/system.cpp
+	${CMAKE_CURRENT_SOURCE_DIR}/Drivers/src/gpio.cpp
+	${CMAKE_CURRENT_SOURCE_DIR}/Drivers/src/flash.cpp
+	${CMAKE_CURRENT_SOURCE_DIR}/Drivers/src/rcc.cpp
+)
+
+# Include directories
+set(include_DIRS
+	${CMAKE_CURRENT_SOURCE_DIR}/inc
+	${CMAKE_CURRENT_SOURCE_DIR}/Drivers/src
+
+	${CMAKE_CURRENT_SOURCE_DIR}/Drivers/CMSIS/Include
+	${CMAKE_CURRENT_SOURCE_DIR}/Drivers/CMSIS/Device/ST/STM32F4xx/Include
+)
+
+# Symbols definition
+set(symbols_c_SYMB 
+
+)
+set(symbols_cxx_SYMB 
+    
+)
+set(symbols_asm_SYMB
+    
+)
+
+# Link directories
+set(link_DIRS
+    
+)
+
+# Link libraries
+set(link_LIBS
+    
+)
