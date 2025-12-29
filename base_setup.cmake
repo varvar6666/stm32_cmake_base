0039a683ac81145f5e718f24ccb4ce5e7bfb8344
Serge <varvar666@gmail.com>
2025-12-29 11:01:05 +0300

    start

--- base_setup.cmake
+++ base_setup.cmake
@@ -27,17 +27,6 @@ set(sources_SRCS
 	${CMAKE_CURRENT_SOURCE_DIR}/no_system_files/sysmem.c
 	${CMAKE_CURRENT_SOURCE_DIR}/no_system_files/syscalls.c
 
-	${CMAKE_CURRENT_SOURCE_DIR}/Drivers/src/system.cpp
-	${CMAKE_CURRENT_SOURCE_DIR}/Drivers/src/gpio.cpp
-	${CMAKE_CURRENT_SOURCE_DIR}/Drivers/src/flash.cpp
-	${CMAKE_CURRENT_SOURCE_DIR}/Drivers/src/rcc.cpp
-	# ${CMAKE_CURRENT_SOURCE_DIR}/Drivers/src/uart.cpp
-	# ${CMAKE_CURRENT_SOURCE_DIR}/Drivers/src/interface.cpp
-	# ${CMAKE_CURRENT_SOURCE_DIR}/Drivers/src/dma.cpp
-	# ${CMAKE_CURRENT_SOURCE_DIR}/Drivers/src/tim.cpp
-	# ${CMAKE_CURRENT_SOURCE_DIR}/Drivers/math/sint.c
-	# ${CMAKE_CURRENT_SOURCE_DIR}/Drivers/FastMathFunctions/arm_sin_f32.c
-
 )
 
 # Include directories
@@ -46,9 +35,6 @@ set(include_DIRS
 
 	${CMAKE_CURRENT_SOURCE_DIR}/download_files/cmsis/Core/Include
 	${CMAKE_CURRENT_SOURCE_DIR}/download_files/Device/Include
-
-	${CMAKE_CURRENT_SOURCE_DIR}/Drivers/src
-	# ${CMAKE_CURRENT_SOURCE_DIR}/Drivers/math
 )
 
 # Symbols definition
